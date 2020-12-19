f0a2b8ee30515701e25cbae4caf7d32e23f12fc8
Ryan Xin <xinpf0715@gmail.com>
2020-12-20 10:31:51 +1100

    footer content added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,33 @@
 import React, { useState, useEffect } from 'react';
+import './Footer.css';
 
 const Footer = (props) => {
   return(
-    <>
-      <h1>Footer</h1>
-    </>
+    <footer className="footer">
+      <div>
+        <div>
+          <img src="/seer_logo_white.png" alt="seer_logo" />
+        </div>
+        <div>
+          <img src="/linkedin_icon.svg" alt="linkedin" />
+          <img src="/facebook_icon.svg" alt="facebook" />
+          <img src="/instagram_icon.svg" alt="instagram" />
+        </div>
+      </div>
+      <div></div>
+      <div>
+        <div>
+          <ul>
+            <li>About Us</li>
+            <li>Privacy Policy</li>
+            <li>Terms of Service</li>
+          </ul>
+        </div>
+        <div>
+          <p>&copy; Seer Data & Analytics 2020</p>
+        </div>
+      </div>
+    </footer>
   )
 }
 
