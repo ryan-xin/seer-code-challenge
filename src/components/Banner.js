d68e586a07a81a314a9547d62b6174fb2ec348f4
Ryan Xin <xinpf0715@gmail.com>
2020-12-20 17:58:07 +1100

    Banner desktop styling finished

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,10 +3,18 @@ import './Banner.css';
 
 const Banner = (props) => {
   return(
-    <div className="banner">
-      <h1>{props.title}</h1>
-      <p>{props.description}</p>
-      <img src='/arrow_down.svg' alt="down arrow" />
+    <div
+      className="banner"
+      style={{
+        background: 'url("/banner_circle_01.svg") left top no-repeat, url("/banner_circle_02.svg") right bottom no-repeat',
+        backgroundColor: '#6C3563'
+      }}
+    >
+      <div className="banner-content">
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
+        <img src='/arrow_down.svg' alt="down arrow" />
+      </div>
     </div>
   )
 }
