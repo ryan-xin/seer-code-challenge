64e45daff15ba203fd9cd26e2705d62d582eeedc
Ryan Xin <xinpf0715@gmail.com>
2020-12-20 11:47:52 +1100

    setup hashrouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Link, HashRouter as Router} from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Blog from './components/Blog';
+import Post from './components/Post';
 
 function App() {
   return (
     <>
-      <Header />
-      <hr />
-      <Blog />
-      <hr />
-      <Footer />
+      <Router>
+        <Header />
+        <hr />
+        <Route exact path="/blog" component={Blog} />
+        <Route exact path="/blog/:postId" component={Post} />
+        <hr />
+        <Footer />
+      </Router>
     </>
   );
 }
---
+++ src/components/Post.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const Post = (props) => {
+  return(
+    <>
+      <h1>Post</h1>
+    </>
+  )
+}
+
+export default Post;
